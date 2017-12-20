b17b39fea9b6990cd6542053849099bc2b124eaf
Raghav Khator <raaghavkhatoor1999@gmail.com>
2017-12-21 00:16:01 +0530

    task Four

---
+++ TaskFour/app/src/main/java/raghavkhator/com/taskfour/MainActivity.java
@@ -0,0 +1,50 @@
+package raghavkhator.com.taskfour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1= (Button) findViewById(R.id.button1);
+        Button b2=(Button) findViewById(R.id.button2);
+        final EditText et1= (EditText) findViewById(R.id.editText1);
+        final EditText et2= (EditText) findViewById(R.id.editText2);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                et1.setText(null);
+                et2.setText(null);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText et1= (EditText) findViewById(R.id.editText1);
+        EditText et2= (EditText) findViewById(R.id.editText2);
+        String MovieName=et1.getText().toString(), Genre=et2.getText().toString();
+        Intent intent1= new Intent(MainActivity.this, SecondActivity.class);
+        intent1.putExtra("MovieName",MovieName);
+        intent1.putExtra("Genre",Genre);
+        startActivity(intent1);
+
+        /*switch(v.getId()){
+            case R.id.button1:
+                break;
+            case R.id.button2:
+        }*/
+    }
+
+}
---
+++ TaskFour/app/src/main/java/raghavkhator/com/taskfour/SecondActivity.java
@@ -0,0 +1,29 @@
+package raghavkhator.com.taskfour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String MovieName = intent.getStringExtra("MovieName"), Genre = intent.getStringExtra("Genre");
+        TextView tv1 = (TextView) findViewById(R.id.textView2);
+        if(!MovieName.isEmpty() || !Genre.isEmpty())
+        {
+
+            tv1.setText("Name of the Movie is " + MovieName + ",\nwhich has a Genre- " + Genre);
+
+        }
+        else
+        {
+            tv1.setTextColor(getResources().getColor(R.color.Red));
+            tv1.setText("PLEASE ENTER A VALID INPUT");
+        }
+    }
+}
